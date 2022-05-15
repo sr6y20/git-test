5807e3faed84a410b0e8923e387c96124582176a
mlddc <1836165455@qq.com>
2022-05-15 19:20:20 +0800

    hot commit

--- src/main/java/com/nih/GitTest.java
+++ src/main/java/com/nih/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("nihao1");
+        System.out.println("nihao.hot-fix");
     }
 }
