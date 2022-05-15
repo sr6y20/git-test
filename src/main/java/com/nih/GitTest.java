8b9b5a165289eb4de11323811c0de40bde6655e4
mlddc <1836165455@qq.com>
2022-05-15 20:44:03 +0800

    push commit

--- src/main/java/com/nih/GitTest.java
+++ src/main/java/com/nih/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("nihao1");
         System.out.println("nihao.hot-fix");
         System.out.println("靠");
+        System.out.println("zailai");
     }
 }
